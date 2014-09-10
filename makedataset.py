9590972b2adb4feb373fc5e4c87391fb9d78598f
codeanticode <andres.colubri@gmail.com>
2014-09-10 14:56:10 -0400

    much faster

--- makedataset.py
+++ makedataset.py
@@ -38,7 +38,7 @@ def init_dataset(output_folder):
     template_file.close()
     project_file.close()
 
-def read_table(data_file, table_file, data, columns, names, titles, types, prefix):
+def read_master(data_file, table_file, data, columns, names, titles, types):
     reader = csv.reader(open(data_file, 'r'), dialect='excel')
     reader.next()
     for row in reader:
@@ -48,11 +48,47 @@ def read_table(data_file, table_file, data, columns, names, titles, types, prefi
     for row in reader:
         if len(row) == 3:
             columns.append(col)
+            names.append(row[0].strip())
+            titles.append(row[1].strip())
+            types.append(row[2].strip())
+        col = col + 1
+
+def read_table(data_file, table_file, data, columns, names, titles, types, idcol, prefix):
+    reader = csv.reader(open(data_file, 'r'), dialect='excel')
+    reader.next()
+    for row in reader:
+        id = row[idcol]
+        if id in data:
+            pdat = data[id]
+        else:
+            pdat = []
+            data[id] = pdat
+        pdat.append(row)
+    reader = csv.reader(open(table_file, 'r'), dialect='excel-tab')
+    col = 0
+    for row in reader:
+        if len(row) == 3:
+            columns.append(col)
             names.append(prefix + row[0].strip())
             titles.append(row[1].strip())
             types.append(row[2].strip())
         col = col + 1
+
+def read_team_names(data_file):
+    # Read team names
+    team_names = {}
+    reader = csv.reader(open(data_file, 'r'), dialect='excel')
+    reader.next()
+    for row in reader:
+        team_names[row[2]] = row[40]
     
+def read_salaries(data_file):
+    reader = csv.reader(open(data_file, 'r'), dialect='excel')
+    reader.next()
+    for row in reader:
+        row[4] = str(float(row[4]) * 19.2)
+        salary_data.append(row)
+ 
 def write_data(filename):
     writer = csv.writer(open(filename, 'w'), dialect='excel-tab')    
     writer.writerow(all_names)
@@ -61,27 +97,30 @@ def write_data(filename):
     lastPercPoint = 0
     for row in master_data:
         mrow = [val if val != '' else '\\N' for val in row]
-        cid = mrow[0]
+        pid = mrow[0]
     
         years_active = Set([])
         yearly_batting_data = {}
         yearly_fielding_data = {}    
         yearly_pitching_data = {} 
     
-        for brow in batting_data:
-            if brow[0] == cid:
+        if pid in batting_data:        
+            brows = batting_data[pid]
+            for brow in brows:
                 year = brow[1]
                 yearly_batting_data[year] = [val if val != '' else '\\N' for val in brow]
                 years_active.add(year)
             
-        for prow in pitching_data:
-            if prow[0] == cid:
+        if pid in pitching_data:        
+            prows = pitching_data[pid]
+            for prow in prows: 
                 year = prow[1]
                 yearly_pitching_data[year] = [val if val != '' else '\\N' for val in prow]
                 years_active.add(year)
             
-        for frow in fielding_data:
-            if frow[0] == cid:
+        if pid in fielding_data:        
+            frows = fielding_data[pid]
+            for frow in frows:
                 year = frow[1]
                 yearly_fielding_data[year] = [val if val != '' else '\\N' for val in frow]
                 years_active.add(year)            
@@ -98,6 +137,7 @@ def write_data(filename):
             stint = '\\N'
             team = '\\N'        
             league = '\\N'
+            salary = '\\N'
                                     
             if year in yearly_batting_data: 
                 brow = yearly_batting_data[year]
@@ -129,10 +169,14 @@ def write_data(filename):
             else:
                 all_row.extend(['\\N'] * len(fielding_columns))
 
+#             if year in yearly_batting_data: 
+                
+
             all_row.insert(len(master_columns), year)
             all_row.insert(len(master_columns) + 1, stint)
             all_row.insert(len(master_columns) + 2, team)        
             all_row.insert(len(master_columns) + 3, league)
+            all_row.insert(len(master_columns) + 4, salary)            
             
             writer.writerow(all_row)
             all_data.append(all_row)
@@ -153,11 +197,11 @@ def write_groups(filename):
     write_xml_line('<data>', xml_file, xml_strings)
     write_xml_line(' <group name="Player info">', xml_file, xml_strings)
     write_xml_line('  <table name="Vitals">', xml_file, xml_strings)
-    for name in master_names[0: len(master_names) - 4]:    
+    for name in master_names[0: len(master_names) - 5]:    
         write_xml_line('   <variable name="' + name + '"/>', xml_file, xml_strings)
     write_xml_line('  </table>', xml_file, xml_strings)
     write_xml_line('  <table name="Teams">', xml_file, xml_strings)
-    for name in master_names[len(master_names) - 4: len(master_names)]:
+    for name in master_names[len(master_names) - 5: len(master_names)]:
         write_xml_line('   <variable name="' + name + '"/>', xml_file, xml_strings)
     write_xml_line('  </table>', xml_file, xml_strings)
     write_xml_line(' </group>', xml_file, xml_strings)
@@ -187,14 +231,7 @@ def write_groups(filename):
         sys.stderr.write('XML validation error:\n')
         raise    
     
-def write_dict(teams_file, dict_file):
-    # Read team names
-    team_names = {}
-    reader = csv.reader(open(teams_file, 'r'), dialect='excel')
-    reader.next()
-    for row in reader:
-        team_names[row[2]] = row[40]
-
+def write_dict(dict_file):
     all_types = []
     all_types.extend(master_types)
     all_types.extend(batting_types)
@@ -280,43 +317,54 @@ master_names = []
 master_titles = []
 master_types = []
 
-batting_data = []
+batting_data = {}
 batting_columns = []
 batting_names = []
 batting_titles = []
 batting_types = []
 
-pitching_data = []
+pitching_data = {}
 pitching_columns = []
 pitching_names = []
 pitching_titles = []
 pitching_types = []
 
-fielding_data = []
+fielding_data = {}
 fielding_columns = []
 fielding_names = []
 fielding_titles = []
 fielding_types = []
 
+team_names = {}
+salary_data = []
+
 init_dataset(output_folder)
 
 print 'Reading master table...'
-read_table(source_folder + 'Master.csv', 'master-table.tsv', master_data, master_columns, master_names, master_titles, master_types, '')    
+read_master(source_folder + 'Master.csv', 'master-table.tsv', master_data, master_columns, master_names, master_titles, master_types) 
 print 'Done.'
-master_names.extend(['yearID', 'stint', 'teamID', 'lgID']);
-master_titles.extend(['Year', "player's stint", 'Team', 'League']);
-master_types.extend(['int', 'int', 'category', 'category']);    
+master_names.extend(['yearID', 'stint', 'teamID', 'lgID', 'salary']);
+master_titles.extend(['Year', "player's stint", 'Team', 'League', 'Salary']);
+master_types.extend(['int', 'int', 'category', 'category', 'float']);
 
 print 'Reading batting table...'
-read_table(source_folder + 'Batting.csv', 'batting-table.tsv', batting_data, batting_columns, batting_names, batting_titles, batting_types, 'BAT.')    
+read_table(source_folder + 'Batting.csv', 'batting-table.tsv', batting_data, batting_columns, batting_names, batting_titles, batting_types, 0, 'BAT.')    
 print 'Done.'
 
 print 'Reading pitching table...'
-read_table(source_folder + 'Pitching.csv', 'pitching-table.tsv', pitching_data, pitching_columns, pitching_names, pitching_titles, pitching_types, 'PITCH.')
+read_table(source_folder + 'Pitching.csv', 'pitching-table.tsv', pitching_data, pitching_columns, pitching_names, pitching_titles, pitching_types, 0, 'PITCH.')
 print 'Done.'
 
 print 'Reading fielding table...'
-read_table(source_folder + 'Fielding.csv', 'fielding-table.tsv', fielding_data, fielding_columns, fielding_names, fielding_titles, fielding_types, 'FIELD.')
+read_table(source_folder + 'Fielding.csv', 'fielding-table.tsv', fielding_data, fielding_columns, fielding_names, fielding_titles, fielding_types, 0, 'FIELD.')
+print 'Done.'
+
+print 'Reading team names...'
+read_team_names(source_folder + 'Teams.csv')
+print 'Done.'
+
+print 'Reading salaries...'
+read_salaries(source_folder + 'Salaries.csv')
 print 'Done.'
 
 all_data = []
@@ -334,6 +382,6 @@ print '  Writing groups file...'
 write_groups(output_folder + 'groups.xml')
 print '  Done.'
 print '  Writing dictionary file...'
-write_dict(source_folder + 'Teams.csv', output_folder + 'dictionary.tsv')
+write_dict(output_folder + 'dictionary.tsv')
 print '  Done.'
 print 'DATASET READY.'
